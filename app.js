2a63514887cb017afe5647a3375ac75eb2af5f54
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-05-14 17:03:54 -0500

    Add multiple products to store and display on page

--- app.js
+++ app.js
@@ -2,14 +2,25 @@
   var app = angular.module("store", []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: "Dodecahedron",
-    price: 2.95,
-    description: "The thinga ma jigga"
-  };
+  var gems = [
+    {
+      name: "Pentagonal Gem",
+      price: 5.95,
+      description: "This is a description",
+      canPurchase:true,
+      soldOut: false,
+    },
+    {
+      name: "Dodecahedron",
+      price: 2.95,
+      description: "The thinga ma jigga",
+      canPurchase: true,
+      soldOut: false,
+    }
+  ];
 
 })();
 
